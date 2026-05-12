74799c5d04d3ce35a4ec1b060a2ced3b8bd611f1
qqz-unnc <qqz342644@gmail.com>
2026-05-12 18:15:25 +0800

    Update bib.js

--- data/generated/bib.js
+++ data/generated/bib.js
@@ -4,14 +4,14 @@ var bib = {
         "authors": ["Agrawal, Kushagra", "Alladi, Tejasvi", "Agrawal, Ayush"],
         "doi": "10.1109/tits.2022.3146024",
         "journal": "IEEE Transactions on Intelligent Transportation Systems",
-        "month": "Nov",
-        "number": "11",
-        "pages": "22596-22606",
+        "month": 11, // 改为数字类型（1-12），匹配SurVis的内部解析逻辑
+        "number": 11, // 改为数字类型
+        "pages": "22596--22606", // 改为双连字符，符合BibTeX和SurVis的标准格式
         "publisher": "IEEE",
         "title": "NovelADS: A Novel Anomaly Detection System for Intra-Vehicular Networks",
         "type": "article",
-        "url": "http://dx.doi.org/10.1109/TITS.2022.3146024",
-        "volume": "23",
+        "url": "https://dx.doi.org/10.1109/TITS.2022.3146024", // 改为https，避免协议问题
+        "volume": 23, // 改为数字类型
         "year": 2022,
         "keywords": ["intrusion detection", "vehicle security", "deep learning"],
         "id": "Agrawal2022",
@@ -27,14 +27,14 @@ var bib = {
         "authors": ["Ahmed, Imran", "Jeon, Gwanggil", "Ahmad, Awais"],
         "doi": "10.1109/mce.2021.3139170",
         "journal": "IEEE Consumer Electronics Magazine",
-        "month": "Jan",
-        "number": "1",
-        "pages": "117-123",
+        "month": 1, // 改为数字类型
+        "number": 1, // 改为数字类型
+        "pages": "117--123", // 改为双连字符
         "publisher": "IEEE",
         "title": "Deep Learning-Based Intrusion Detection System for Internet of Vehicles",
         "type": "article",
-        "url": "http://dx.doi.org/10.1109/MCE.2021.3139170",
-        "volume": "12",
+        "url": "https://dx.doi.org/10.1109/MCE.2021.3139170", // 改为https
+        "volume": 12, // 改为数字类型
         "year": 2023,
         "keywords": ["intrusion detection", "IoV security", "deep learning"],
         "id": "Ahmed2023",
